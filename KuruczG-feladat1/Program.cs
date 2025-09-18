141500ffa1130a6284043e9a8d3e8f0f25e80b70
Kurucz Gergő <KuruczG@KKSZKI.LOCAL>
2025-09-18 08:16:44 +0200

    C# projekt is created

---
+++ KuruczG-feladat1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuruczG_feladat1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adjon meg egy számot: ");
+            int szam = int.Parse(Console.ReadLine());
+
+            if (szam % 2 == 0)
+            {
+                Console.WriteLine("A szám páros.");
+            }
+            else
+            {
+                Console.WriteLine("A szám páratlan.");
+            }
+
+        }
+    }
+}
