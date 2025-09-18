01536f664a73b5e05d349f0f2a03ea4ee430c884
Kurucz Gergő <KuruczG@KKSZKI.LOCAL>
2025-09-18 08:32:04 +0200

    Feladat3 is ready

--- KuruczG-feladat1/Program.cs
+++ KuruczG-feladat1/Program.cs
@@ -22,7 +22,7 @@ namespace KuruczG_feladat1
                 Console.WriteLine("A szám páratlan.");
             }
 
-            //2. feladat
+        //2. feladat
             if (szam > 0)
             {
                 Console.WriteLine("A szám pozitív.");
@@ -36,9 +36,31 @@ namespace KuruczG_feladat1
                 Console.WriteLine("A szám nulla.");
             }
 
-             
-            
+        //3. feladat 
+            Console.Write("Adjon meg egy számot (1-12) között és megmondom melyik évszakban van: ");
+            int honap = int.Parse(Console.ReadLine());
+            if (honap == 12 || honap == 1 || honap == 2)
+            {
+                Console.WriteLine("Tél");
+            }
+            else if (honap >= 3 && honap <= 5)
+            {
+                Console.WriteLine("Tavasz");
+            }
+            else if (honap >= 6 && honap <= 8)
+            {
+                Console.WriteLine("Nyár");
+            }
+            else if (honap >= 9 && honap <= 11)
+            {
+                Console.WriteLine("Ősz");
+            }
+            else
+            {
+                Console.WriteLine("Hibás hónap szám!");
+            }
 
+            
 
 
 
